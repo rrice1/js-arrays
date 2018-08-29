c93fb778e5440fba9f784dfd478df9d7af52eb9c
Robert Rice <rrice1291@gmail.com>
2018-08-28 19:09:26 -0500

    added mini challenge

--- arrays.js
+++ arrays.js
@@ -1,1 +1,26 @@
-console.log('hello')
+const cats = ['fluffy', 'spike', 'killer'];
+console.log('1st cat', cats[0]);
+
+const favoriteAnimal = 'cow,bear,dog,cat';
+const favoriteAnimalArray = favoriteAnimal.split(','); //['c', 'o', 'w']
+console.log('favoriteAnimalArray', favoriteAnimalArray);
+
+const numArray = [1,2,3,4,5];
+const newNum = numArray.join('cow');
+console.log('newNum', newNum);
+
+// mini challenges
+
+const palChecker = (word) => {
+    const wordArray = word.split('').join('');
+    const reverseArray = word.split('').reverse().join('');
+
+ if(wordArray === reverseArray){
+     console.log(`${word} IS a palindrome`);
+ } else {
+     console.log(`${word} is NOT a palindrome`);
+ }
+};
+
+palChecker('mom');
+palChecker('hgfdsfgn');
